56454fbefcf1a0ae5cc8d93fb7f4f5ec44805bba
Paul Ellis <paul@ellisfoundation.com>
2021-02-26 09:25:35 -0800

    Improve code readability

--- domEventToCSSVariable.js
+++ domEventToCSSVariable.js
@@ -3,26 +3,23 @@
   dom-event-to-css-variable.js: expose element event values (offsetX, nodeName, etc) to CSS
   © 2021 Paul Ellis
   License: MIT
-  Version: 1.1
+  Version: 1.1.1
   */
 
   function domEventToCSSVariable(el, prop, opts) {
-    const eventOpts = (opts ? Object.assign({}, opts) : {});
-    eventOpts.passive = true;
-    eventOpts.event = eventOpts.event || 'mousemove';
+    const eventOpts = Object.assign({}, opts);
+    eventOpts.passive = true; // using `e.preventDefault()` has no effect if passive === true. Improves performance for some events.
+    eventOpts.event   = eventOpts.event || 'mousemove';
 
     const transformFn = (eventOpts.transform ? eventOpts.transform : typeCoerce);
-    const targetEl = (eventOpts.target ? eventOpts.target : el);
+    const targetEl    = (eventOpts.target    ? eventOpts.target    : el);
 
     const baseName = eventOpts.cssVarName || prop;
     const propName = `--${camelToKebabCase(baseName)}`;
 
     function eventFn(e) {
-      const transformedVal = transformFn(
-        isDefined(e[prop]) ?
-        e[prop] :
-        e.target[prop], el, e
-      );
+      const whichProp = (isDefined(e[prop]) ? e[prop] : e.target[prop]);
+      const transformedVal = transformFn(whichProp, el, e);
 
       const val = (
         eventOpts.stringify ?
@@ -36,17 +33,9 @@
     el.addEventListener(eventOpts.event, eventFn, eventOpts);
   }
 
-  function camelToKebabCase(s) {
-    return s.replace(/[A-Z]/g, c => `-${c.toLowerCase()}`);
-  }
-
-  function isDefined(v) {
-    return typeof v !== 'undefined';
-  }
-
-  function typeCoerce(v) {
-    return (v == +v ? +v : v);
-  }
+  const camelToKebabCase = (s) => s.replace(/[A-Z]/g, c => `-${c.toLowerCase()}`);
+  const isDefined        = (v) => typeof v !== 'undefined';
+  const typeCoerce       = (v) => (v == +v ? +v : v);
 
   global.domEventToCSSVariable = domEventToCSSVariable;
 })(this);
